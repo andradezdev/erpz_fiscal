991f928aa2a1a3431f6c5b6c8e5e04074248bfe2
ERPZ Dev <dev@erpz.io>
2026-09-18 16:48:16 +0000

    feat: Adiciona botao de impressao do Cupom NFC-e 80mm diretamente na tela do PDV (Point of Sale)

--- erpz_fiscal/public/js/erpz_fiscal.js
+++ erpz_fiscal/public/js/erpz_fiscal.js
@@ -24,3 +24,45 @@ $(document).ready(function() {
         }
     } catch(e) {}
 });
+
+// Integracao Oficial ERPZ Fiscal com o PDV (NFC-e 80mm)
+function setup_pos_nfce_integration() {
+    if (!window.erpnext || !erpnext.PointOfSale || !erpnext.PointOfSale.PastOrderSummary) return;
+    if (erpnext.PointOfSale.PastOrderSummary._erpz_fiscal_patched) return;
+    erpnext.PointOfSale.PastOrderSummary._erpz_fiscal_patched = true;
+
+    const original_load_summary = erpnext.PointOfSale.PastOrderSummary.prototype.load_summary_of;
+    erpnext.PointOfSale.PastOrderSummary.prototype.load_summary_of = function(doc, after_submission = false) {
+        original_load_summary.call(this, doc, after_submission);
+        
+        const me = this;
+        setTimeout(() => {
+            if (!me.$summary_btns) return;
+            
+            if (me.$summary_btns.find(".nfce-btn").length === 0) {
+                const btn_html = $(`
+                    <div class="summary-btn btn btn-primary nfce-btn mr-2" style="background: #16a34a; border-color: #15803d; color: white; font-weight: bold; cursor: pointer;">
+                        <i class="octicon octicon-file-text mr-1"></i> Imprimir Cupom NFC-e (80mm)
+                    </div>
+                `);
+                
+                btn_html.on("click", function() {
+                    const inv_name = me.doc ? me.doc.name : (doc ? doc.name : null);
+                    if (inv_name) {
+                        window.open('/api/method/erpz_fiscal.api.nfe.imprimir_danfe?pos_invoice=' + encodeURIComponent(inv_name));
+                    }
+                });
+                
+                me.$summary_btns.prepend(btn_html);
+            }
+        }, 150);
+    };
+}
+
+$(document).on("page-change", function() {
+    setTimeout(setup_pos_nfce_integration, 500);
+});
+
+$(document).ready(function() {
+    setTimeout(setup_pos_nfce_integration, 500);
+});
